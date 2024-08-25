0cb900602972d02545c469723df766dae9406fe5
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-08-25 12:25:00 -0500

    Update Converter.java

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -2,11 +2,28 @@ package com.csc;
 
 public class Converter {
 
+  // This function converts ounces to pounds. The return is pounds as a decimal
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+    double conversion = ounces/16.0;
+    if(conversion == 1.0) {
+      return conversion + "lb";
+    }
+    else {
+      return conversion + "lbs";
+    }
   }
 
+  // This function converts ounces to pounds and ounces. The return is a formatted lbs and oz ints. 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
-  }
+    int remainingOunces = ounces % 16;
+    int pounds = ounces / 16;
+    if(pounds == 1) {
+      return pounds + " lb " + remainingOunces + " oz";
+    }
+    else {
+      return pounds + " lbs " + remainingOunces + " oz";
+    }
+  } 
+
 }
+
